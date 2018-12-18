d852d9a416716c06ab9ad56024219788f438d46b
Larrygf02 <raulgf_02@hotmail.com>
2018-12-18 13:30:51 -0500

    Thymeleaf bootstrap

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+	
+	@Value("${application.controllers.mensaje}")
+	private String mensaje;
 	@GetMapping("/index")
 	public String hola(Model model) {
+		model.addAttribute("mensaje", mensaje);
 		return "hola";
 	}
 }
